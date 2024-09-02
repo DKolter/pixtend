db7dc5000ac88dd7dff2009c443d838d535fb1bb
DKolter <danielkolter157@gmail.com>
2024-09-03 00:23:54 +0200

    Fix retain example

--- examples/retain.rs
+++ examples/retain.rs
@@ -7,11 +7,5 @@ fn main() {
     pixtend.set_retain_enable(true);
     pixtend.set_retain_data(vec![1, 2, 3, 4, 5]).unwrap();
     pixtend.read_write().unwrap();
-    pixtend.set_retain_enable(false);
-
-    loop {
-        pixtend.read_write().unwrap();
-        println!("{:?}", pixtend.get_retain_data().unwrap());
-        std::thread::sleep(std::time::Duration::from_secs(1));
-    }
+    println!("{:?}", pixtend.get_retain_data().unwrap());
 }
